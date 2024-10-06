dc01c4e117d050d13445dda10f6ed52fa8e89058
Siedelwood <119606836+Siedelwood@users.noreply.github.com>
2024-10-06 16:42:33 +0200

    Update base.lua
    Version number QSB.Version updated

--- qsb/lua/modules/QSB_0_Kernel/base.lua
+++ qsb/lua/modules/QSB_0_Kernel/base.lua
@@ -17,8 +17,7 @@ API = {};
 SCP = {Core = {}};
 
 QSB = {};
-QSB.Version = "3.0.0.beta-1.1.12 - 16.05.2024";
-
+QSB.Version = "3.0.0.beta-2.1.2 - 06.10.2024"
 ---
 -- Stellt wichtige Kernfunktionen bereit.
 --
