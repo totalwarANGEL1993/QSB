86423097b85fdf1472a73cfe327f6d3ea05deebb
schmeling65 <markusrumpel2@gmail.com>
2024-11-25 13:41:37 +0100

    Neues Loggin für Konsole eingefügt und das Setzen des LogLevels nur bei validem Parameter.

--- qsb/lua/modules/QSB_0_Kernel/debug.lua
+++ qsb/lua/modules/QSB_0_Kernel/debug.lua
@@ -334,10 +334,9 @@ end
 --
 function API.ActivateDebugMode(_CheckAtRun, _TraceQuests, _DevelopingCheats, _DevelopingShell, _LogLevel)
     Swift.Debug:ActivateDebugMode(_CheckAtRun, _TraceQuests, _DevelopingCheats, _DevelopingShell);
-    if _LogLevel == nil then
-        _LogLevel = 4
+    if _LogLevel then
+        API.SetLogLevel(_LogLevel, _LogLevel)
     end
-    API.SetLogLevel(_LogLevel, _LogLevel)
 end
 
 -- Aktiviert DisplayScriptErrors, auch wenn der Benutzer das Spiel nicht
